d8a2ce8ad7073c9302231e13b026d93a5978a9b7
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2020-03-08 23:12:52 +0100

    server: Add basic transfer loop

--- server/src/kobla-base-server.cpp
+++ server/src/kobla-base-server.cpp
@@ -1,5 +1,18 @@
+#include <ncnet/Server.h>
+
 #include <iostream>
 
+using namespace std;
+
+void parse_incoming_packets(ncnet::Transfer &transfer) {
+    cout << "Got packet from ID " << transfer.get_connection_id() << endl;
+}
+
 int main() {
-    return 0;
+    ncnet::Server server;
+
+    server.start("", 10000);
+    server.run_transfer_loop(parse_incoming_packets);
+
+    server.stop();
 }
