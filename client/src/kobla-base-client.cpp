c8dd7138251920acfaf9847112a1160163460c4d
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2020-03-09 19:39:49 +0100

    client: Add basic SFML window

--- client/src/kobla-base-client.cpp
+++ client/src/kobla-base-client.cpp
@@ -1,14 +1,52 @@
 #include <ncnet/Client.h>
 
+#include <SFML/Graphics.hpp>
+
+using namespace ncnet;
+using namespace sf;
 using namespace std;
 
-int main() {
-    ncnet::Client client;
-    client.start("localhost", 10000);
+bool run_engine(RenderWindow &window, Client &client) {
+    // Render loop
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+                return true;
+            }
+        }
 
-    ncnet::Packet echo;
-    echo << "Hello!";
-    client.send_packet(echo);
-    this_thread::sleep_for(1000ms);
+        window.clear();
+        // Draw everything
+        window.display();
+    }
+
+    return true;
+}
+
+bool connect_server(Client &client) {
+    // TODO: Register transfer loop?
+    return client.start("localhost", 11112);
+}
+
+bool load_engine(RenderWindow &window) {
+    window.setVerticalSyncEnabled(true);
+    // TODO: Add loading
+    return true;
+}
+
+int main() {
+    RenderWindow window(VideoMode(640, 480), "kobla-base-client");
+    if (!load_engine(window)) {
+        return -1;
+    }
+
+    Client client;
+    if (!connect_server(client)) {
+        // Fine for now
+    }
+
+    run_engine(window, client);
     client.stop();
 }
